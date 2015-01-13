8e8ef0d240f1776a70e0652b64c5ffdbe1ece318
Matteo Muscella <matteo.muscella@usi.ch>
2015-01-13 19:17:42 +0100

    Some MPD options, changed Irssi to not show password anymore.

--- sh/options.zsh
+++ sh/options.zsh
@@ -27,5 +27,5 @@ zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p
 zstyle ':completion:*' use-compctl false
 zstyle ':completion:*' verbose true
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+zstyle ':completion:*:kill:*' command 'ps -c -u $USER -o pid,%cpu,cputime,state,comm'
 
