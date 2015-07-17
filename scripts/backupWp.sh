e16ed6dd1fe7998889aa66e9f5c0e76a0520412b
Matteo Muscella <matteo.muscella@usi.ch>
2015-07-17 22:09:29 +0200

    GNU Stow clean before first commit

--- scripts/backupWp.sh
+++
@@ -1,5 +0,0 @@
-# Backup Wordpress Files
-tar -jcvf ./wordpress_backup_$(date +%y%m%d).tbz2 /var/www/*
-
-# Backup Wordpress Database
-mysqldump --opt -u root -p<password> wordpress > wordpress_backup_$(date +%y%m%d).sql
